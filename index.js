4b722a0e3caf03bdc9447b7898fffaac9321f2b0
Eamonn O'Brien-Strain <eob@well.com>
2023-01-15 18:39:04 -0800

    Reformat to standard style

--- index.js
+++ index.js
@@ -7,159 +7,167 @@ import {
   section,
   span,
   time,
-  video,
-} from "https://unpkg.com/ez-html-elements";
+  video
+} from 'https://unpkg.com/ez-html-elements'
 
-const SERVER_KEY = "server";
-let server = localStorage.getItem(SERVER_KEY);
+/* global timelineElement noServerElement headerElement serverElement */
 
-function dateView(dateString) {
-  const ms = Date.now() - Date.parse(dateString);
+const SERVER_KEY = 'server'
+let server = window.localStorage.getItem(SERVER_KEY)
+
+function dateView (dateString) {
+  const ms = Date.now() - Date.parse(dateString)
   if (ms < 1500) {
-    return `${ms} ms`;
+    return `${ms} ms`
   }
-  const seconds = Math.round(ms / 1000);
+  const seconds = Math.round(ms / 1000)
   if (seconds < 90) {
-    return `${seconds} seconds`;
+    return `${seconds} seconds`
   }
-  const minutes = Math.round(seconds / 60);
+  const minutes = Math.round(seconds / 60)
   if (minutes < 90) {
-    return `${minutes} minutes`;
+    return `${minutes} minutes`
   }
-  const hours = Math.round(minutes / 60);
+  const hours = Math.round(minutes / 60)
   if (hours < 90) {
-    return `${hours} hours`;
+    return `${hours} hours`
   }
-  const days = Math.round(hours / 24);
-  return `${days} days`;
+  const days = Math.round(hours / 24)
+  return `${days} days`
 }
 
 const dateHtml = (dateString) =>
-  time({ datetime: dateString }, dateView(dateString) + " ago");
+  time({ datetime: dateString }, dateView(dateString) + ' ago')
 
-function accountHtml(account) {
-  const { avatar, username, url, display_name, followers_count } = account;
-  const accountServer = url.match(/https:\/\/([^\/]+)\//)[1];
-  const avatarSize = Math.sqrt(followers_count);
+function accountHtml (account) {
+  const {
+    avatar,
+    username,
+    url,
+    display_name: displayName,
+    followers_count: followersCount
+  } = account
+  const accountServer = url.match(/https:\/\/([^/]+)\//)[1]
+  const avatarSize = Math.sqrt(followersCount)
+
   return (
     img({ src: avatar, width: avatarSize, height: avatarSize }) +
     span(
-      " @" +
+      ' @' +
         username +
-        img({ src: `https://${accountServer}/favicon.ico` }, ["favicon"])
+        img({ src: `https://${accountServer}/favicon.ico` }, ['favicon'])
     ) +
-    display_name
-  );
+    displayName
+  )
 }
 
-function attachementHtml(attachement) {
-  const { type, preview_url, meta, description } = attachement;
+function attachementHtml (attachement) {
+  const { type, preview_url: previewUrl, meta, description } = attachement
   if (!meta.small) {
-    return "";
+    return ''
   }
-  const { width, height } = meta.small;
+  const { width, height } = meta.small
   switch (type) {
-    case "image":
+    case 'image':
       return figure(
-        img({ alt: description, src: preview_url, width, height }) +
+        img({ alt: description, src: previewUrl, width, height }) +
           figcaption(description)
-      );
-    case "video":
+      )
+    case 'video':
       return figure(
-        video({ controls: true, src: preview_url, width, height }) +
+        video({ controls: true, src: previewUrl, width, height }) +
           figcaption(description)
-      );
+      )
   }
 }
 
-const attachementListHtml = (as) => as.map(attachementHtml).join("");
+const attachementListHtml = (as) => as.map(attachementHtml).join('')
 
-function statusHtml(status) {
+function statusHtml (status) {
   const {
-    created_at,
+    created_at: createdAt,
     content,
     account,
-    media_attachments,
-    in_reply_to_account_id,
-  } = status;
+    media_attachments: attachments,
+    in_reply_to_account_id: inReplyToAccountId
+  } = status
   const mediaSection =
-    media_attachments && media_attachments.length > 0
-      ? section(attachementListHtml(media_attachments))
-      : "";
-  const contentSection = section(p(dateHtml(created_at)), p(content));
+    attachments && attachments.length > 0
+      ? section(attachementListHtml(attachments))
+      : ''
+  const contentSection = section(p(dateHtml(createdAt)), p(content))
   const accountSection =
-    in_reply_to_account_id === account.id
-      ? ""
-      : section(["metadata"], accountHtml(account));
-  return accountSection + contentSection + mediaSection;
+    inReplyToAccountId === account.id
+      ? ''
+      : section(['metadata'], accountHtml(account))
+  return accountSection + contentSection + mediaSection
 }
 
 /** Recursive */
-async function statusChain(status) {
-  const { in_reply_to_id } = status;
-  if (!in_reply_to_id) {
-    return statusHtml(status);
+async function statusChain (status) {
+  const { in_reply_to_id: inReplyToId } = status
+  if (!inReplyToId) {
+    return statusHtml(status)
   }
   try {
     const response = await fetch(
-      `https://${server}/api/v1/statuses/${in_reply_to_id}`
-    );
-    const inReplyTo = await response.json();
-    return (await statusChain(inReplyTo)) + "🧵" + statusHtml(status);
+      `https://${server}/api/v1/statuses/${inReplyToId}`
+    )
+    const inReplyTo = await response.json()
+    return (await statusChain(inReplyTo)) + '🧵' + statusHtml(status)
   } catch {
-    return statusHtml(status);
+    return statusHtml(status)
   }
 }
 
-async function showTimeline(querySuffix) {
+async function showTimeline (querySuffix) {
   const response = await fetch(
     `https://${server}/api/v1/timelines/${querySuffix}`
-  );
-  const statuses = await response.json();
-  timelineElement.replaceChildren();
+  )
+  const statuses = await response.json()
+  timelineElement.replaceChildren()
   for (const status of statuses) {
-    const { in_reply_to_id } = status;
     timelineElement.insertAdjacentHTML(
-      "beforeend",
+      'beforeend',
       article(await statusChain(status))
-    );
+    )
   }
 }
 
-async function hasServer() {
-  noServerElement.classList.add("hidden");
-  headerElement.innerHTML = server;
-  if (!location.hash) {
-    location.hash = "#public";
+async function hasServer () {
+  noServerElement.classList.add('hidden')
+  headerElement.innerHTML = server
+  if (!document.location.hash) {
+    document.location.hash = '#public'
   } else {
-    app();
+    app()
   }
 }
 
-async function app() {
-  switch (location.hash) {
-    case "#public":
-      await showTimeline("public?limit=40");
-      break;
-    case "#public/local":
-      await showTimeline("public?limit=40&local=true");
-      break;
+async function app () {
+  switch (document.location.hash) {
+    case '#public':
+      await showTimeline('public?limit=40')
+      break
+    case '#public/local':
+      await showTimeline('public?limit=40&local=true')
+      break
   }
 }
 
-window.onhashchange = app;
+window.onhashchange = app
 
 if (server) {
-  hasServer();
+  hasServer()
 } else {
-  noServerElement.classList.remove("hidden");
-  serverElement.addEventListener("keyup", async (event) => {
-    if (event.key === "Enter") {
-      server = serverElement.value.trim();
+  noServerElement.classList.remove('hidden')
+  serverElement.addEventListener('keyup', async (event) => {
+    if (event.key === 'Enter') {
+      server = serverElement.value.trim()
       if (server && server.match(/[a-z]+\.[a-z]+/)) {
-        localStorage.setItem(SERVER_KEY, server);
-        await hasServer();
+        window.localStorage.setItem(SERVER_KEY, server)
+        await hasServer()
       }
     }
-  });
+  })
 }
